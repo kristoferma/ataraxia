ef826c48313fe338dedfe5377e8c31a12c71e2cc
Kristófer Másson <kristoferma@me.com>
2024-03-11 22:04:11 +0000

    add colors

--- app/page.tsx
+++ app/page.tsx
@@ -3,12 +3,20 @@
 import React from "react";
 const INITIAL_NUMBER_OF_HEXAGONS = 1000;
 
+const colors = ["yellow", "red", "blue"];
+
+const randomColor = () => {
+  if (Math.random() > 0.6) {
+    return colors[Math.floor(Math.random() * colors.length)];
+  } else return "";
+};
+
 export default function Home() {
   return (
     <main className="main">
       <div className="hex-container">
         {[...Array(INITIAL_NUMBER_OF_HEXAGONS)].map((e, i) => (
-          <div key={i}></div>
+          <div className={randomColor()} key={i}></div>
         ))}
       </div>
     </main>
