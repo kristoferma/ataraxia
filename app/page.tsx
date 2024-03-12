71a1dffe2d3a2fb47ededd41b29ee06e2dd21a9e
Kristófer Másson <kristoferma@me.com>
2024-03-12 23:23:10 +0000

    change color on click

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 const INITIAL_NUMBER_OF_HEXAGONS = 500;
 
-const colors = ["yellow", "red", "blue"];
+const colors = ["#ffcb05", "#ed1c24", "#0088cb"];
 
 const randomColor = () => {
   if (Math.random() > 0.6) {
@@ -13,15 +13,15 @@ const randomColor = () => {
 
 export default function Home() {
   return (
-    <main className="main relative">
+    <main className="main flex flex-col justify-center relative w-screen h-screen">
       <div className="background">
         <div className="hex-container">
           {[...Array(INITIAL_NUMBER_OF_HEXAGONS)].map((e, i) => (
-            <div className={randomColor()} key={i}></div>
+            <Hexagon key={i} />
           ))}
         </div>
       </div>
-      <div className="absolute flex w-screen h-screen justify-center items-center">
+      <div className="absolute flex w-auto h-auto self-center">
         <div className="bg-white p-10 rounded-full border-2 border-black font-black text-2xl md:text-9xl">
           Ataraxia
         </div>
@@ -29,3 +29,14 @@ export default function Home() {
     </main>
   );
 }
+
+const Hexagon = () => {
+  const [color, setColor] = React.useState<string | undefined>(() =>
+    randomColor()
+  );
+  const changeColor = React.useCallback(() => {
+    setColor(colors[Math.floor(Math.random() * colors.length)]);
+  }, []);
+
+  return <div onClick={changeColor} style={{ backgroundColor: color }}></div>;
+};
