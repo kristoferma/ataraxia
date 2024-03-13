a20da588883477877973beebba46b0ef9fd43893
Kristófer Másson <kristoferma@me.com>
2024-03-13 20:04:15 +0000

    add static image background for ios

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 import React from "react";
-const INITIAL_NUMBER_OF_HEXAGONS = 500;
+import Image from "next/image";
+
+import hexagons from "../public/hexagon-tiles.png";
+
+const INITIAL_NUMBER_OF_HEXAGONS = 1000;
 
 const colors = ["#ffcb05", "#ed1c24", "#0088cb"];
 
@@ -14,13 +18,7 @@ const randomColor = () => {
 export default function Home() {
   return (
     <main className="main flex flex-col justify-center relative w-screen h-screen">
-      <div className="background">
-        <div className="hex-container">
-          {[...Array(INITIAL_NUMBER_OF_HEXAGONS)].map((e, i) => (
-            <Hexagon key={i} />
-          ))}
-        </div>
-      </div>
+      <HexagonBackground />
       <div className="absolute flex w-auto h-auto self-center">
         <div className="bg-white p-10 rounded-full border-2 border-black font-black text-2xl md:text-9xl">
           Ataraxia
@@ -29,6 +27,40 @@ export default function Home() {
     </main>
   );
 }
+
+const HexagonBackground = () => {
+  if (typeof window !== "undefined") {
+    const userAgent = window.navigator.userAgent;
+
+    if (userAgent.match(/iPad/i) || userAgent.match(/iPhone/i)) {
+      return (
+        <div className="background">
+          <Image
+            alt="White hexagon grid with few hexagons colored in red, yellow and blue."
+            src={hexagons}
+            placeholder="blur"
+            quality={100}
+            fill
+            sizes="200vw"
+            priority
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
+      );
+    }
+  }
+  return (
+    <div className="background">
+      <div className="hex-container">
+        {[...Array(INITIAL_NUMBER_OF_HEXAGONS)].map((e, i) => (
+          <Hexagon key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 const Hexagon = () => {
   const [color, setColor] = React.useState<string | undefined>(() =>
